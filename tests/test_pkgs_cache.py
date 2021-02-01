1621ffc5733c8f600c2406d14418b456492b5afb
Vladimir Timofeenko <id@vtimofeenko.com>
2021-02-01 12:31:05 -0800

    Added Remote link generation mechanism
    Added method to generate a link for remote, to be used with main's
    output.
    
    Added exception handling logic to directory processing so that cache
    building would not break if one remote is bad.

--- tests/test_pkgs_cache.py
+++ tests/test_pkgs_cache.py
@@ -9,13 +9,30 @@ def test_process_directory(has_ebuild, result, monkeypatch, create_pkgdir):
     if has_ebuild:
         create_ebuild(create_pkgdir)
     if result is not None:
-        result = pc.Package(create_pkgdir, '', ['1'], [], '', '')
-    monkeypatch.setattr(pc, 'get_atomname', lambda _: "")
-    monkeypatch.setattr(pc, 'get_versions', lambda _: ["1"])
-    monkeypatch.setattr(pc, 'get_upstreams', lambda _: ("", ""))
-    monkeypatch.setattr(pc, 'get_short_description', lambda _: "")
-    monkeypatch.setattr(pc, 'get_long_description', lambda _: "")
+        result = pc.Package(create_pkgdir, "", ["1"], [], "", "")
+
+    monkeypatch.setattr(pc, "get_atomname", lambda _: "")
+    monkeypatch.setattr(pc, "get_versions", lambda _: ["1"])
+    monkeypatch.setattr(pc, "get_upstreams", lambda _: ("", ""))
+    monkeypatch.setattr(pc, "get_short_description", lambda _: "")
+    monkeypatch.setattr(pc, "get_long_description", lambda _: "")
     assert pc.process_directory(create_pkgdir) == result
+
+    if result is not None:
+        # check exception handling
+        def _raise(_):
+            raise Exception
+
+        monkeypatch.setattr(pc, "get_atomname", _raise)
+
+        # Suppress stderr printing
+        class NullWriter:
+            def write(self, s):
+                pass
+
+        monkeypatch.setattr(pc, "stderr", NullWriter())
+        # Make sure that no exception is raised, None is returned.
+        assert pc.process_directory(create_pkgdir) is None
 
 
 @pytest.mark.parametrize("iterable,result", (([0, 1, 2, None, 3, False], [1, 2, 3]),))
@@ -29,5 +46,15 @@ def mock_process_directory(_):
 
 
 def test_build_pkgs_cache(tmp_path, monkeypatch, create_pkgdir):
-    monkeypatch.setattr(pc, 'process_directory', mock_process_directory)
+    monkeypatch.setattr(pc, "process_directory", mock_process_directory)
     assert pc.build_pkgs_cache(tmp_path) == ["pkg"]
+
+
+def test_remote_link():
+    r = pc.Remote("github", "gentoo-mirror/gentoo")
+    assert r.remote_link() == "https://github.com/gentoo-mirror/gentoo"
+    r = pc.Remote("pypi", "typer")
+    assert r.remote_link() == "https://pypi.org/project/typer"
+    r = pc.Remote("something else", "typer")
+    with pytest.raises(NotImplementedError):
+        r.remote_link()
